d8a9dc14c1a8cde0777a23b30d674c759df886d9
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2025-04-17 16:43:50 -0500

    search filters, menu and submenus

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,14 +1,44 @@
+import configPromise from '@payload-config';
+import { getPayload } from 'payload';
+
 import { Navbar } from './navbar';
 import { Footer } from './footer';
+import { SearchFilters } from './search-filters';
+import { Category } from '@/payload-types';
 
 interface Props {
   children: React.ReactNode;
 }
 
-const Layout = ({ children }: Props) => {
+const Layout = async ({ children }: Props) => {
+  const payload = await getPayload({
+    config: configPromise
+  });
+
+  const data = await payload.find({
+    collection: 'categories',
+    depth: 1, // if having problems querying increase. https://payloadcms.com/docs/queries/depth subcategories.[0] will ve of type Category. If depth is set to 0 will be strings and break everything.
+    pagination: false, // can change if there become too many
+    where: {
+      parent: {
+        exists: false
+      }
+    }
+  });
+
+  // simplifying data structure.
+  const formattedData = data.docs.map((doc) => ({
+    ...doc,
+    subcategories: (doc.subcategories?.docs ?? []).map((doc) => ({
+      // Because of 'depth 1' we are confident doc will be of type Category
+      ...(doc as Category)
+    }))
+  }));
+
   return (
     <div className="flex flex-col min-h-screen">
       <Navbar />
+      <SearchFilters data={formattedData} />
       <div className="flex-1 bg-[#F4F4F0]">{children}</div>
       <Footer />
     </div>
--- src/app/(app)/(home)/page.tsx
+++ src/app/(app)/(home)/page.tsx
@@ -1,20 +1,7 @@
-import configPromise from '@payload-config';
-import { getPayload } from 'payload';
-
-export default async function Home() {
-  const payload = await getPayload({
-    config: configPromise
-  });
-
-  const data = await payload.find({
-    collection: 'categories'
-  });
-
+export default function Home() {
   return (
     <div className="p-4">
-      <div className="flex flex-col gap-y-4">
-        {JSON.stringify(data, null, 2)}
-      </div>
+      <div className="flex flex-col gap-y-4">Homepage</div>
     </div>
   );
 }
