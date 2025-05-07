9d166960445a542ee0df0463b68ab7c73d785420
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2025-05-07 10:25:04 -0500

    products added

---
+++ src/collections/Products.ts
@@ -0,0 +1,41 @@
+import { CollectionConfig } from 'payload';
+
+export const Products: CollectionConfig = {
+  slug: 'products',
+  fields: [
+    {
+      name: 'name',
+      type: 'text',
+      required: true
+    },
+    {
+      name: 'description',
+      type: 'text'
+    },
+    {
+      name: 'price',
+      type: 'number',
+      required: true,
+      admin: {
+        description: 'In USD'
+      }
+    },
+    {
+      name: 'category',
+      type: 'relationship',
+      relationTo: 'categories',
+      hasMany: false // one product can only belong to one category
+    },
+    {
+      name: 'image',
+      type: 'upload',
+      relationTo: 'media'
+    },
+    {
+      name: 'refundPolicy',
+      type: 'select',
+      options: ['30 days', '14 days', '7 days', '1 day', 'no refunds'],
+      defaultValue: '30 days'
+    }
+  ]
+};
