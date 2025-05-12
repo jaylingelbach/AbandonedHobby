f833f748b74128dc4c3bdb80c4d8b3d06504a5a8
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2025-05-12 11:15:20 -0500

    added product card views and loading functionality

--- src/modules/products/server/procedures.ts
+++ src/modules/products/server/procedures.ts
@@ -1,14 +1,17 @@
 import z from 'zod';
 import type { Sort, Where } from 'payload';
-import { Category } from '@/payload-types';
+import { Category, Media } from '@/payload-types';
 
 import { baseProcedure, createTRPCRouter } from '@/trpc/init';
 import { sortValues } from '../search-params';
+import { DEFAULT_LIMIT } from '@/constants';
 
 export const productsRouter = createTRPCRouter({
   getMany: baseProcedure
     .input(
       z.object({
+        cursor: z.number().default(1),
+        limit: z.number().default(DEFAULT_LIMIT),
         category: z.string().nullable().optional(),
         minPrice: z.string().nullable().optional(),
         maxPrice: z.string().nullable().optional(),
@@ -90,9 +93,16 @@ export const productsRouter = createTRPCRouter({
         collection: 'products',
         depth: 1, // will populate category and image
         where,
-        sort
+        sort,
+        page: input.cursor,
+        limit: input.limit
       });
-      // await new Promise((resolve) => setTimeout(resolve, 1000));
-      return data;
+      return {
+        ...data,
+        docs: data.docs.map((doc) => ({
+          ...doc,
+          image: doc.image as Media | null // settings types so we can get imageURL in product list
+        }))
+      };
     })
 });
