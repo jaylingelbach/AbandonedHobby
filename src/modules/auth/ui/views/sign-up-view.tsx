a88144f619b20234115714504296447daa7759c0
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2025-06-04 22:59:38 -0500

    updated recap and import of z from zod

--- src/modules/auth/ui/views/sign-up-view.tsx
+++ src/modules/auth/ui/views/sign-up-view.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import Link from 'next/link';
-import z from 'zod';
+import { z } from 'zod';
 import { Poppins } from 'next/font/google';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { toast } from 'sonner';
