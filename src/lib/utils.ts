b3ea0862461710290e43017efa67b3300d9b75da
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2025-06-06 17:25:31 -0500

    changes added to help test without subdomains enabled

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -7,9 +7,9 @@ export function cn(...inputs: ClassValue[]) {
 
 export function generateTenantURL(tenantSlug: string) {
   const isDev = process.env.NODE_ENV === 'development';
-  const isSubdomainRoutingEnabled = Boolean(
-    process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING
-  );
+  const isSubdomainRoutingEnabled =
+    process.env.NEXT_PUBLIC_ENABLE_SUBDOMAIN_ROUTING === 'true';
+
   const protocol = isDev || isSubdomainRoutingEnabled ? 'http' : 'https';
   let domain = process.env.NEXT_PUBLIC_ROOT_DOMAIN;
 
