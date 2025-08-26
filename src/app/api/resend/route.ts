bb04fb46eaaa8e8243865fefa00d687df243d6d2
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2025-08-26 18:59:07 -0500

    small change

--- src/app/api/resend/route.ts
+++ src/app/api/resend/route.ts
@@ -64,7 +64,8 @@ export async function POST(req: NextRequest) {
     await payload.sendEmail({
       to: email,
       subject,
-      html
+      html,
+      from: process.env.POSTMARK_FROM_NAME!
       // Optionally set from; defaults come from your nodemailerAdapter config:
       // from: 'Jay from abandoned hobby <jay@abandonedhobby.com>',
     });
