bfa37b1ce6c13f0a27251e11cb6867ed65cc209f
Jay-at-193694594941 <jay.lingelbach@gmail.com>
2025-12-02 12:39:10 -0600

    Add link to home in checkout

--- src/modules/checkout/ui/components/navbar.tsx
+++ src/modules/checkout/ui/components/navbar.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 
 import { Button } from '@/components/ui/button';
 import { generateTenantURL } from '@/lib/utils';
-
+import { HomeIcon } from 'lucide-react';
 interface Props {
   slug: string;
 }
@@ -11,6 +11,9 @@ export const Navbar = ({ slug }: Props) => {
   return (
     <nav className="h-20 border-b font-medium bg-white">
       <div className="max-w-(--breakpoint-xl) mx-auto flex justify-between items-center h-full px-4 lg:px-12">
+        <Link href="/">
+          <HomeIcon />
+        </Link>
         <p className="text-xl">Checkout</p>
         <Button asChild variant="elevated">
           <Link href={generateTenantURL(slug)}>Continue Shopping</Link>
